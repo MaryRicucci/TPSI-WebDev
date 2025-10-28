12a6b5e04798038e75aca6835de05cfd14d94876
MaryRicucci <maria.ricucci.12@gmail.com>
2025-10-28 11:44:41 +0100

    Implement number search in array with user input
    Added functionality to check if a number is in an array and display the result.

--- Esercizio8/script.js
+++ Esercizio8/script.js
@@ -3,4 +3,23 @@ Crea un array di numeri e un input in cui l’utente scrive un numero. Quando si
 il bottone, il programma controlla se quel numero è presente nell’array e mostra
 “Numero trovato!” oppure “Numero non presente.
 */
-let button = document.getElementById("bt
+let button = document.getElementById("btn");
+let number= document.getElementById("val");
+let out = document.getElementById("out");
+
+let array= [33,55,12,63,16,44,23];
+button.addEventListener("click" , view);
+
+function view () {
+  let f = false ;
+  let n = Number (number.value) ;
+  for (let i=0;i<array.length; i++) {
+    if (n==array[i]) {
+      out.textContent="Numero trovato!" ;
+      break ;
+    }
+    
+  }
+  
+  
+}
