7f46098bc6b629eed6dd9f971e27f08fd4ecbc06
MaryRicucci <maria.ricucci.12@gmail.com>
2025-10-28 11:39:08 +0100

    Create script.js for number presence check
    Add initial script for number search functionality

---
+++ Esercizio8/script.js
@@ -0,0 +1,6 @@
+/*
+Crea un array di numeri e un input in cui l’utente scrive un numero. Quando si clicca
+il bottone, il programma controlla se quel numero è presente nell’array e mostra
+“Numero trovato!” oppure “Numero non presente.
+*/
+let button = document.getElementById("bt
